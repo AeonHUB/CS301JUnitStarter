8fe8f0f62eefe4b82510724abda577333e2ce852
Grant Nelson <nelsong22@up.edu>
2020-03-23 20:44:12 -0700

    check 2

--- app/src/main/java/up/edu/dimcalc/TwoPoints.java
+++ app/src/main/java/up/edu/dimcalc/TwoPoints.java
@@ -4,6 +4,8 @@ import android.graphics.Point;
 
 import java.util.Random;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * This class is designed to perform operations on two points in 2D space
  */
@@ -69,31 +71,6 @@ public class TwoPoints {
 
         result = xDiff / yDiff;
         return result;
-    }
-    /** when created, getPoint() should show both points at the origin */
-    @Test
-    public void getPoint() throws Exception {
-        TwoPoints testPoints = new TwoPoints();
-        Point p1 = testPoints.getPoint(0);
-        Point p2 = testPoints.getPoint(1);
-        assertEquals(0, p1.x);
-        assertEquals(0, p1.y);
-        assertEquals(0, p2.x);
-        assertEquals(0, p2.y);
-    }
-
-    /** verify that arbitrary values are properly stored via setPoint() */
-    @Test
-    public void setPoint() throws Exception {
-        TwoPoints testPoints = new TwoPoints();
-        testPoints.setPoint(0, 5, -3);
-        testPoints.setPoint(1, -3, 5);
-        Point p1 = testPoints.getPoint(0);
-        Point p2 = testPoints.getPoint(1);
-        assertEquals(5, p1.x);
-        assertEquals(-3, p1.y);
-        assertEquals(-2, p2.x);
-        assertEquals(5, p2.y);
     }
 
 
